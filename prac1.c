c54e317457fa0f15072d4046b56a578e2794c2a0
Ricardo Castro Gonzáles <ricardo.castro27@uabc.edu.mx>
2024-02-14 17:23:28 -0800

    Prac1
    Added bitwise operations to solve

---
+++ prac1.c
@@ -0,0 +1,44 @@
+/******************************************************************************
+Examples code
+*******************************************************************************/
+
+#include <stdio.h>
+#include <inttypes.h>
+/*
+  Ejercicios de enmascaramientos. 
+  Lo que se busca es que imprima el valor que esta comentado,
+  alterando unicamente el valor anterior utilizando los operadores bitwise.
+  Todas las conversiones DEBEN ser realizadas con una sola operacion como se muestra
+  en el siguiente ejemplo
+*/
+int main()
+{
+    uint16_t a = 0;
+    printf("\na = %X", a);
+//  Como ejemplo se da el analisis de la primera operacion
+
+//1 B0B0 
+//       hex           bin
+//  a = 0000 = 0000 0000 0000 0000  (valor original)
+//      B0B0 = 1011 0000 1011 0000  (valor que se desea)
+//  Observamos que todas las diferencias radica en que los bits tienen que estar habilitados,
+//  por lo tanto usaremos el operador OR (|) para convertir esos bits en '1's
+
+//    0000 0000 0000 0000  (valor original)
+// OR 1011 0000 1011 0000  (mascara para lograrlo) = (1<<15) | (3<<12) | (1<<7) | (3<<4)
+//------------------------------------------------
+//    1011 0000 1011 0000  (valor que se desea)
+    a |= (1<<15) | (3<<12) | (1<<7) | (3<<4); //realizado en una sola línea
+    printf("\na = %X", a);
+    
+//2 C0C0
+//3 CECE
+//4 BEBE
+//5 FE00
+//6 FEAA
+//7 0155
+//8 0001
+//9 1000
+
+    return 0;
+}
